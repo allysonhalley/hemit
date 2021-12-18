9ca59ff4c1b92946003bfd7022668d09216c90a9
Allyson Halley <allysonhalley@gmail.com>
2021-12-18 20:16:32 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+import Hello from './components/Hello/Hello'
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>H&F</h1>
+        <p>tech intelligence</p>
       </header>
+      <Hello />
     </div>
   );
 }
---
+++ src/components/Hello/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hello = () => {
+    return (
+        <div>
+            <h1> Wellcome to the World of the Tech Intelligence! </h1>
+        </div>
+    )
+}
+
+export default Hello;
