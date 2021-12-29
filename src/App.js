74676f80782416647cbdd1fd111ca8ff802950a0
Allyson Halley <allysonhalley@gmail.com>
2021-12-29 18:11:40 -0300

    add auto deploy aws s3

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from './components/Header/Header'
 import Hello from './components/Hello/Hello'
+import Portfolio from './components/Portifolio/Portfolio'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Hello />
+      <Portfolio />
     </div>
   );
 }
---
+++ src/components/Portifolio/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Portfolio.css';
+
+const Portfolio = () => {
+    return (        
+        <div className="App-portfolio">                        
+            <h3>Comming Soon</h3>
+        </div>        
+      );
+}
+
+export default Portfolio;
